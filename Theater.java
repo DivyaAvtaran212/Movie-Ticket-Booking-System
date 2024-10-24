f152b00e94a6ab14b59ac1c55b7727470f699814
Divya Avtaran <109351482+DivyaAvtaran212@users.noreply.github.com>
2024-10-24 18:27:22 +0530

    Theater.java

--- Theater.java
+++ Theater.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class Theater {
-    private String Location;
+    private String location;
     private int SeatingCapacity;
 
     private DatabaseOperation db = new DatabaseOperation();
 
     public void insertTheater(String location,int SeatingCapacity){
-        this.Location = location;
+        this.location = location;
         this.SeatingCapacity = SeatingCapacity;
-        String sql = "INSERT INTO theaters (Location, SeatingCapacity) VALUES (?,?)";
-        Object[] values = {Location,SeatingCapacity};
+        String sql = "INSERT INTO theaters (location, SeatingCapacity) VALUES (?,?)";
+        Object[] values = {location,SeatingCapacity};
         int rowsAffected = db.executeUpdate(sql, values);
         if(rowsAffected>0)
             System.out.println("Theater inserted successfully");
@@ -22,8 +22,8 @@ public class Theater {
         String sql = "SELECT * from theaters";
         List<Map<String,Object>> theaters = db.getRecords(sql);
         for (Map<String, Object> theater : theaters) {
-            System.out.println("Theater ID: " + theater.get("TheaterID"));
-            System.out.println("Location: " + theater.get("Location"));
+            System.out.println("Theater ID: " + theater.get("TheatreID"));
+            System.out.println("Location: " + theater.get("location"));
             System.out.println("Seating Capacity: " + theater.get("SeatingCapacity"));
             System.out.println("-----------------------------");
             
